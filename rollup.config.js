6e4241539a20402dfed750e09f79e3acf8fc6830
pandegong <674948122@qq.com>
2023-10-17 13:29:31 +0800

    feat✨:

--- rollup.config.js
+++ rollup.config.js
@@ -4,15 +4,15 @@ export default {
         {
             file: "dist/index.esm.js",
             format: "es",
-        },
-        {
-            file: "dist/index.cjs.js",
-            format: "cjs",
-        },
-        {
-            file: "dist/index.umd.js",
-            format: "umd",
-            name: "PdgTool",
-        },
+        }
+        // {
+        //     file: "dist/index.cjs.js",
+        //     format: "cjs",
+        // },
+        // {
+        //     file: "dist/index.umd.js",
+        //     format: "umd",
+        //     name: "PdgTool",
+        // },
     ],
 };
