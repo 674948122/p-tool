91a298a7b232cc2af1a064613e037e58a217650d
pandegong <674948122@qq.com>
2024-09-10 10:00:53 +0800

    fix:

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,16 @@
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+
 export default {
     input: "src/index.js",
     output: [
         {
             file: "dist/index.esm.js",
-            format: "es",
-        }
-        // {
-        //     file: "dist/index.cjs.js",
-        //     format: "cjs",
-        // },
-        // {
-        //     file: "dist/index.umd.js",
-        //     format: "umd",
-        //     name: "PdgTool",
-        // },
+            format: "esm",
+        },
     ],
+    plugins: [resolve(), commonjs(), babel(), terser()],
+    mode: "production",
 };
